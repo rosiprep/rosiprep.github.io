61035d6e1a14329f231a649fddfd58a308cf52e6
Gil Hur <gil.hur@sf.snu.ac.kr>
2021-03-03 19:34:43 +0900

    Remove the debug code

--- check.js
+++ check.js
@@ -1,5 +1,5 @@
 var domain = document.referrer.replace('http://','').replace('https://','').split(/[/?#]/)[0];
-alert(domain);
+//alert(domain);
 if (!(domain == "rosiprep.github.io" || domain == "blog.naver.com")) {
   window.location.replace("https://blog.naver.com/PostView.nhn?blogId=rosi2505&logNo=222263267738&parentCategoryNo=&categoryNo=37&viewDate=&isShowPopularPosts=false&from=postList");
 }
