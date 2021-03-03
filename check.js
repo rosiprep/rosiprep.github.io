2acdd7ba485917791749a979adfc7eb104517f7b
Gil Hur <gil.hur@sf.snu.ac.kr>
2021-03-03 21:28:36 +0900

    Remove debug code

--- check.js
+++ check.js
@@ -1,11 +1,11 @@
-alert(document.referrer);
-alert(navigator.userAgent);
+// alert(document.referrer);
+// alert(navigator.userAgent);
 if ((!document.referrer || document.referrer == "") &&
     !navigator.userAgent.includes("NAVER")) {
   window.location.replace("https://blog.naver.com/PostView.nhn?blogId=rosi2505&logNo=222263267738&parentCategoryNo=&categoryNo=37&viewDate=&isShowPopularPosts=false&from=postList");
 }
 // var domain = document.referrer.replace('http://','').replace('https://','').split(/[/?#]/)[0];
-//alert(domain);
+// alert(domain);
 // if (!(domain == "rosiprep.github.io" || domain == "blog.naver.com" || domain == "m.blog.naver.com")) {
 //   window.location.replace("https://blog.naver.com/PostView.nhn?blogId=rosi2505&logNo=222263267738&parentCategoryNo=&categoryNo=37&viewDate=&isShowPopularPosts=false&from=postList");
 // }
