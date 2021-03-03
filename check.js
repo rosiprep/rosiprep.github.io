43aaea69d3f28bd385e31aed882a9b61ccdf2aad
Gil Hur <gil.hur@sf.snu.ac.kr>
2021-03-03 19:32:42 +0900

    Bug fix

--- check.js
+++ check.js
@@ -1,9 +1,4 @@
-if (document.referrer) {
-    url = document.referrer;
-} else {
-    url = "";
-}
-domain = (new URL(url)).hostname;
+var domain = document.referrer.replace('http://','').replace('https://','').split(/[/?#]/)[0];
 alert(domain);
 if (!(domain == "rosiprep.github.io" || domain == "blog.naver.com")) {
   window.location.replace("https://blog.naver.com/PostView.nhn?blogId=rosi2505&logNo=222263267738&parentCategoryNo=&categoryNo=37&viewDate=&isShowPopularPosts=false&from=postList");
