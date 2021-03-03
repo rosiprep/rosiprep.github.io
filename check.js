3470f47d205c403fef2218cd8fe827cd45d8bd31
Gil Hur <gil.hur@sf.snu.ac.kr>
2021-03-03 21:14:30 +0900

    Generalize domain checking

--- check.js
+++ check.js
@@ -1,6 +1,9 @@
-//alert(document.referrer);
-var domain = document.referrer.replace('http://','').replace('https://','').split(/[/?#]/)[0];
-//alert(domain);
-if (!(domain == "rosiprep.github.io" || domain == "blog.naver.com" || domain == "m.blog.naver.com")) {
+alert(document.referrer);
+if (!document.referrer || document.referrer == "") {
   window.location.replace("https://blog.naver.com/PostView.nhn?blogId=rosi2505&logNo=222263267738&parentCategoryNo=&categoryNo=37&viewDate=&isShowPopularPosts=false&from=postList");
 }
+// var domain = document.referrer.replace('http://','').replace('https://','').split(/[/?#]/)[0];
+//alert(domain);
+// if (!(domain == "rosiprep.github.io" || domain == "blog.naver.com" || domain == "m.blog.naver.com")) {
+//   window.location.replace("https://blog.naver.com/PostView.nhn?blogId=rosi2505&logNo=222263267738&parentCategoryNo=&categoryNo=37&viewDate=&isShowPopularPosts=false&from=postList");
+// }
